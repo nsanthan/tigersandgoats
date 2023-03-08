636490641e3673d5bfcb543d0333a80fccd3406f
nsanthan <nsanthan@hawaii.edu>
2023-03-07 14:21:36 -1000

    Update game-ai_vs_ai.py

--- game-ai_vs_ai.py
+++ game-ai_vs_ai.py
@@ -18,19 +18,12 @@ from huligutta import *
 from functions import *
 from PIL import ImageTk, Image
 from random import randint, choice
-from math import exp
-from csv import writer
-from openpyxl import load_workbook
 
 # Board parts
-sys.setrecursionlimit(10000000)
 numPosition = 23 
 boardSize = 500
 
 Board().clearBoard()
-
-#globalArr = []
-global globalVar
 
 # Game modes:
 #   
@@ -38,9 +31,6 @@ global globalVar
 class Main():
 
     def __init__(self,mode):
-
-    
-        self.reward_arr = []        
         # For self.turn (is it the computer's turn?) 
         self.turn = False
         # self.move is used when a piece is used to move from one location to another. True
@@ -130,14 +120,13 @@ class Main():
         printAndLog('========================')
         attempts = textCount('Attempts: ')
         printAndLog('Attempts: ' +  str(attempts))
-        #printAndLog('Game Mode: '+ mode)
-        #printAndLog('========================')
+        printAndLog('Game Mode: '+ mode)
+        printAndLog('========================')
 
     def button_position(self,pos):
         
         # Called whenever a button is clicked. pos is the location of the  
         # button clicked. For example, if button b0 is clicked, it calls button_position(self,'b0').
-
 
         self.moveCount_prev = self.moveCount
 
@@ -152,14 +141,8 @@ class Main():
 
         if self.moveCount_prev != self.moveCount:
             self.update()
-            #self.collectData()
-        
-        #self.window.mainloop() 
-
-        self.button_position('b1') 
-        self.window.mainloop() #Go to update? this just loops game
-
-                       
+            self.collectData()
+        self.window.mainloop()                
 
     def pvpMode(self,pos):
         # How goats move
@@ -443,108 +426,45 @@ class Main():
                 
     def ai_vs_aiMode(self,pos):
 
-
-        boardToNumber = {                         'b0': 0, 
-                  'a1': 1 , 'b1': 2 , 'c1': 3 , 'd1': 4 , 'e1': 5 , 'f1': 6 , 
-                  'a2': 7 , 'b2': 8 , 'c2': 9 , 'd2': 10, 'e2': 11, 'f2': 12,
-                  'a3': 13, 'b3': 14, 'c3': 15, 'd3': 16, 'e3': 17, 'f3': 18,
-                            'b4': 19, 'c4': 20, 'd4': 21, 'e4': 22
-        }
-
-
-        if self.turn == False:
+       if self.turn == False:
             emptyPos = emptyPositions(Board().boardPositions)
             goats = goatPositions(Board().boardPositions)
 
+            if self.goatCount == 15:
+                goatPos = choice(goats)
 
-            if self.goatCount == 15: #Phase 2 of game
-                
-                #Loop through all goats
-                #Loop through all possible moves
-                #Dictionary {goat#: move location}
-                #iterating through the dictionary
-                #get matrices
+                while len(Goat(goatPos).possibleMoves()) == 0:
+                    if len(goats) != 0:
+                        goats.remove(goatPos)
+                    else:
+                        return
 
-                costArrayOut = {}
-                
-                goodMoveList = {}
+                goatChoice = choice(Goat(goatPos).possibleMoves())
 
-                potentialMoveMat = Board().boardToMatrix()
-                minVal = 100000000000
-                totalCost = 0
-                for x in goats:
-                    costArrayOut[x] = {}
-                    if len(x) != 0:
-                        for y in Goat(x).possibleMoves():
-                            costArrayOut[x][y] = Goat.potential_move(potentialMoveMat,boardToNumber[x],boardToNumber[y]) 
-                            if minVal >= costArrayOut[x][y]:      
-                                if minVal != costArrayOut[x][y]:
-                                    goodMoveList.clear()
-                                minVal = costArrayOut[x][y]
-                                goodMoveList[x] = y
+                if pos in goatPos:
+                    del goatChoice[goatChoice.index(pos)]
 
+                if goatChoice in Position(goatPos[0],goatPos[1]).get_neighbors():
+                    goatMoveFlag = Goat(goatPos).move(goatChoice) 
 
-                            #print("DEBUG BBBBBB\n")
-                        #costArrayOut.clear()
-                        #costArrayIn[x][min(costArrayOut, key=costArrayOut.get)] = min(costArrayOut)
-                #print("BUBUBUBUB\n",costArrayOut.values())    
-                for x in costArrayOut.values():
-                    if len(x) != 0:
-                        for b in x.values():
-                            totalCost += b
-                    
-                #print(minVal/totalCost)
-                self.reward_arr.append(minVal/totalCost)
-                #self.reward_arr.append(exp(-(minVal/totalCost)))
-                #print(minV)
-                #goatChoice = choice(Goat(goatPos).possibleMoves())
+                    if goatMoveFlag == 1:
+                        self.move = False
+                        self.turn = True
+                        self.moveCount = self.moveCount + 1
+                        return
 
-                #if pos in goatPos:
-                #    del goatChoice[goatChoice.index(pos)]
-                
-                z = choice(list(goodMoveList.keys()))
-
-                #if goatChoice in Position(goatPos[0],goatPos[1]).get_neighbors():
-                goatMoveFlag = Goat(z).move(goodMoveList[z]) 
-
-                    #if goatMoveFlag == 1:
-                self.move = False
-                self.turn = True
-                self.moveCount = self.moveCount + 1
-                return
-
-                #else:
-                #    print('Error')
-                #    self.move = False
-                #    self.location = ''
-                #    self.turn = False
-            else: #Phase 1 of game
-
-                ogmatrix = Board().boardToMatrix()
-                cost_array = {}
-                good_moves = [] #Array that will hold all lowest cost moves
-
-                for x in emptyPos: #For every state get all potential moves as matrices conver them into costs and add them to an array of costs
-                    mat2 = ogmatrix.copy()
-                    mat2[boardToNumber[x]] = [0, 1, 0] 
-                    cost_array[x] = Goat.testCostFunction(mat2)
-                z = sum(list(cost_array.values())) #Get the total cost of all possible moves
-                
-                #Go through entire cost_array and find the best move
-                for y in cost_array:
-                    if cost_array[y] == cost_array[min(cost_array, key=cost_array.get)]:
-                        good_moves.append(y)
-
-                #Keep track of the value of the lowest cost move (move that will be chosen)
-                self.reward_arr.append((cost_array[min(cost_array, key=cost_array.get)])/z)
-
-                #Choose a move randomly assuming that there are multiple lowest cost moves
-                goatChoice = choice(good_moves)
+                else:
+                    print('Error')
+                    self.move = False
+                    self.location = ''
+                    self.turn = False
+            else:
+                goatChoice = choice(emptyPos)
                 Goat(goatChoice).place()
                 self.move = False
                 self.turn = True
 
-        if self.turn == True:
+       if self.turn == True:
             numTigers = 3
             possibleCaptures = {}
             tigers = tigerPositions(Board().boardPositions)
@@ -553,7 +473,7 @@ class Main():
                 if Position(tiger[0],tiger[1]).get_captures() != None:
                     for capture in Position(tiger[0],tiger[1]).get_captures():
                         possibleCaptures[tiger] = capture
-            #print(possibleCaptures)
+            print(possibleCaptures)
 
             tigerPos = choice(tigers)
         
@@ -573,13 +493,13 @@ class Main():
                     break
                 
             tigerChoice = choice(Tiger(tigerPos).possibleMoves())
-            #if pos in tigerPos:
-            #    del tigerChoice[tigerChoice.index(pos)]
+            if pos in tigerPos:
+                del tigerChoice[tigerChoice.index(pos)]
 
             if len(possibleCaptures) != 0:
                 tigerPos,where2go = choice(list(possibleCaptures.items()))
                 if Tiger(tigerPos).capture(where2go) == 1:
-                    #print('Possible Captures: ', possibleCaptures, 'Actual: ',tigerPos, where2go)
+                    print('Possible Captures: ', possibleCaptures, 'Actual: ',tigerPos, where2go)
                     self.goatEaten = self.goatEaten + 1
                     self.move = False
                     self.location = ''
@@ -588,7 +508,7 @@ class Main():
             elif tigerChoice in Position(tigerPos[0],tigerPos[1]).get_neighbors():
                 tigerMoveFlag = Tiger(tigerPos).move(tigerChoice) 
                 if tigerMoveFlag == 1:
-                    #print('Tiger from ', tigerPos, ' to ', tigerChoice)
+                    print('Tiger from', tigerPos, ' to ', tigerChoice)
                     self.move = False
                     self.turn = False
                     self.moveCount = self.moveCount + 1
@@ -598,10 +518,8 @@ class Main():
                 self.move = False
                 self.location = ''
                 self.turn = True
-        
 
     def update(self):
-        global globalVar
         # Updates the Screen
         possible_pos = ['a1','a2','a3','b0','b1','b2','b3','b4','c1','c2','c3','c4','d1','d2','d3','d4','e1','e2','e3','e4','f1','f2','f3']
         numGoats = 0
@@ -627,57 +545,14 @@ class Main():
         self.turnDisplay()
         self.selectedToggle()
 
-
-
         # Endgame
-        if possibleMovesCount == 0 or self.goatEaten == 5 or self.moveCount >= 30:
+        if possibleMovesCount == 0:
+            messagebox.showinfo("Game Over: Goat wins") 
+            return
 
-
-            n = []
-            n.append(np.average(self.reward_arr[len(self.reward_arr)-5:len(self.reward_arr)])) #Get average of last 5 goat moves
-            if self.goatEaten == 5 or self.moveCount >= 30:
-                n.append("L")
-            elif possibleMovesCount == 0:
-                n.append("W")
-            n.append(globalVar)
-            n.append(self.moveCount)
-            
-            #with open('filetest.txt', 'a') as f:
-                #f.write(str(n))
-            #wb = load_workbook('work.xlsx')
-        
-
-            #ws = wb.active
-
-            #ws.append(n)
-
-            #wb.save("C:\\Users\\gteoc\\Dropbox\\Reinforcement Learning (1)\\work.xlsx")
-
-            #Restart Game
-            if globalVar < 200:
-                globalVar+=1
-                self.window.destroy()
-                mode = 'ai_vs_ai'     # ai vs ai
-                Board().clearBoard()
-                #os.system("python game-ai_vs_ai.py")
-                game = Main(mode)
-                game.start()
-                return
-            else:
-                #os.system("python game-ai_vs_ai.py")
-                #self.window.destroy()
-                #Board().clearBoard()
-                return
-
-        #if possibleMovesCount == 0:
-            #messagebox.showinfo("Game Over: Goat wins") 
-            #self.window.destroy()
-            #return
-
-        #if self.goatEaten == 5:
-            #messagebox.showinfo("Game Over: Tiger wins")
-            #self.window.destroy()
-            #return
+        if self.goatEaten == 5:
+            messagebox.showinfo("Game Over: Tiger wins") 
+            return
 
     def collectData(self):
         printAndLog("Move: " + str(self.moveCount))
@@ -688,8 +563,6 @@ class Main():
         printAndLog(str(Board().boardPositions))
         editDistance = edit_distance(Board().boardPositions)
         printAndLog("Edit distance: " + str(editDistance))
-        #print("AHHHHHHHHHH")
-        #printAndLog("Reward Arr " + str(self.reward_arr))
         
     def selectedToggle(self):
         if self.move == True:
@@ -767,16 +640,15 @@ class Main():
             emptyPos = choice(empty)
             Goat(emptyPos).place()
             self.turn = True
-        self.update()               #Game will update if button is press 
-        self.button_position('b0')  #Mimic button press (GOES TO Button_Position function)
-        
+        self.update()
+        self.window.mainloop()        
 
-if __name__ == "__main__": #Start of game
-
+if __name__ == "__main__":
+    
     # mode = 'pvp'          # Player vs. Player
-    mode = 'ai_vs_ai'     # ai vs ai
-    # mode = 'goatPlayer'   # Human player is Goat, computer plays Tiger
+    # mode = 'ai_vs_ai'     # ai vs ai
+    mode = 'goatPlayer'   # Human player is Goat, computer plays Tiger
     # mode = 'tigerPlayer'  # Human player is Tiger, computer plays Goat
 
     game = Main(mode)
-    game.start()    #Go to start function
+    game.start()
